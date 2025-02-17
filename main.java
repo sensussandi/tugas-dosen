be6431435c37c53a678a798b6445df1a3d22415d
SENSUS SANDI <sensussandi74@gmail.com>
2025-02-17 19:13:35 +0700

    Commit kedua: Menambahkan file main.java

---
+++ main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        helloWorld();
+    }
+
+    public static void helloWorld() {
+        System.out.println("Hello, world!");
+    }
+}

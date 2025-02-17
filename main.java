3cbdb4d0ee38292e1f772ad1101c93cb6c5c6397
SENSUS SANDI <sensussandi74@gmail.com>
2025-02-17 19:15:58 +0700

    commit ketiga: menambahkan fungsi greeting di main,java

--- main.java
+++ main.java
@@ -1,9 +1,15 @@
 public class Main {
     public static void main(String[] args) {
         helloWorld();
+        greetings("Mahasiswa");
     }
 
     public static void helloWorld() {
         System.out.println("Hello, world!");
     }
+
+    public static void greetings(String name) {
+        System.out.println("Hello, " + name + "!");
+    }
 }
+    
